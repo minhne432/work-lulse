f5dccbe1686e195625984c06181fd8c651ae7cc4
minh <nh.minh0403@gmail.com>
2025-04-26 12:55:37 +0700

    init

---
+++ src/main/java/com/workpulse/workpulse/WorkPulseApplication.java
@@ -0,0 +1,13 @@
+package com.workpulse.workpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workpulse/workpulse/WorkPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workpulse.workpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
